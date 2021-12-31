042dfbd4a03fe17e9d5235fd4cfae28154fccb0e
binhphancreator <binhpv.B18PT031@stu.ptit.edu.vn>
2021-12-31 15:33:06 +0700

    update router

--- bin/app.js
+++ bin/app.js
@@ -7,20 +7,20 @@ var session = require('express-session');
 var RedisStore = require('connect-redis')(session)
 var Redis = require("ioredis");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var webRouter = require('../routes/web');
+var apiRouter = require('../routes/api');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(path.dirname(__dirname), 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(path.dirname(__dirname), 'public')));
 app.use(session({
   store: new RedisStore({client: new Redis(6379, 'minichat-redis')}),
   secret: 'minichat',
@@ -28,8 +28,8 @@ app.use(session({
   saveUninitialized: true,
 }));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', webRouter);
+app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
