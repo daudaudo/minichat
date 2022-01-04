b94813b014fafd08f115bda2f72a221a80f12bfe
binhphancreator <binhpv.B18PT031@stu.ptit.edu.vn>
2022-01-04 16:09:28 +0700

    update serve use cookieParser

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,10 @@
 module.exports = {
   apps: [{
-    script: 'node ./bin/serve > storage/logs/serve.log',
+    script: './bin/serve',
     watch: '.',
     autorestart: false,
     env: {
       NODE_ENV: 'development',
     },
-    env_production: {
-      NODE_ENV: 'production'
-    }
   }],
 };
