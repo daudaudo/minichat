c6e0353ad21148a58f1cbcf10a84920ad7b96b54
binhphancreator <binhpv.B18PT031@stu.ptit.edu.vn>
2022-01-06 14:50:15 +0700

    update tailwind

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,5 +6,7 @@ module.exports = {
     env: {
       NODE_ENV: 'development',
     },
+  }, {
+    script: 'npx tailwindcss -i public/stylesheets/tailwind.css -o public/stylesheets/style.css --watch',
   }],
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  content: [],
+  content: ["./views/**/*.ejs"],
   theme: {
     extend: {},
   },
