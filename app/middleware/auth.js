4a7d161fadb0f7d3eda091c549a90e019d24fde7
binhphancreator <binhpv.B18PT031@stu.ptit.edu.vn>
2022-01-10 16:20:33 +0700

    update auth middleware

--- app/middleware/auth.js
+++ app/middleware/auth.js
@@ -1,4 +1,12 @@
+/**
+ * 
+ * @param {import("express").Request} req 
+ * @param {import("express").Response} res 
+ * @param {import("express").NextFunction} next 
+ */
 module.exports = function(req, res, next)
 {
-    
+    if(req.session.user.id == undefined)
+        res.redirect('/');
+    next();
 }
