dc569888e992dbbe8b607933421a3a34a030f896
binhphancreator <binhpv.B18PT031@stu.ptit.edu.vn>
2022-01-10 09:18:11 +0700

    update header

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@ module.exports = {
   content: ["./views/**/*.ejs"],
   theme: {
     extend: {},
+    minHeight: (theme) => ({
+      auto: 'auto',
+      ...theme('spacing'),
+    })
   },
   plugins: [],
 }
