f13f56aae28daf56df46bcf78cb529bfd66e5d9a
binhphancreator <binhpv.B18PT031@stu.ptit.edu.vn>
2022-01-10 17:39:09 +0700

    add unique for user model

--- app/model/User.js
+++ app/model/User.js
@@ -5,10 +5,12 @@ const userSchema = new mongoose.Schema({
         type: String,
         index: true,
         required: true,
+        unique: true,
     },
     'email': {
         type: String,
         required: true,
+        unique: true,
     },
     'password': {
         type: String,
