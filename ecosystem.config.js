415b41bf379de9f01361cf0c500b81f2c57cdc01
binhphancreator <binhpv.B18PT031@stu.ptit.edu.vn>
2022-01-18 17:31:37 +0700

    add footer

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,11 +1,7 @@
 module.exports = {
   apps: [{
     script: 'npm start',
-    watch: true,
     autorestart: true,
-    env: {
-      NODE_ENV: 'development',
-    },
   }, {
     script: 'npx tailwindcss -i public/stylesheets/build.css -o public/stylesheets/style.css --watch',
   }],
