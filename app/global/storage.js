56abdbcb27387a713bb70acec228625592a3dd4a
binhphancreator <binhpv.B18PT031@stu.ptit.edu.vn>
2022-01-21 17:35:43 +0700

    put file

--- app/global/storage.js
+++ app/global/storage.js
@@ -1,6 +1,10 @@
 var fs = require('fs');
 var path = require('path');
 var uuid = require('uuid');
+var axios = require('axios').default;
+const util = require('util');
+const stream = require('stream');
+const pipeline = util.promisify(stream.finished);
 
 /**
  * @param {String} filename
@@ -18,7 +22,7 @@ function storagePath(filename) {
  * @param {String} src 
  * @param {String} filename 
  */
-function save(src, filename) {
+function saveSync(src, filename) {
   if(!filename) filename = uuid.v4();
   try {
     fs.cpSync(src, storagePath(filename));
@@ -29,7 +33,46 @@ function save(src, filename) {
   }
 }
 
+/**
+ * 
+ * @param {String} content 
+ * @param {String} filename 
+ */
+function putSync(content, filename) {
+  if(!filename) filename = uuid.v4();
+  try {
+    fs.writeFileSync(storagePath(filename), content);
+    return true;
+  } catch (err) {
+    throw err;
+  }
+}
+
+/**
+ * 
+ * @param {String} url 
+ * @param {String} filename 
+ */
+async function putFromUrl(url, filename)
+{
+  try {
+    if(!filename) filename = uuid.v4();
+    var res = await axios.get(url, {responseType: 'stream'});
+    if(res.status == 200) {
+      var file = fs.createWriteStream(storagePath(filename));
+      res.data.pipe(file);
+      await pipeline(file);
+      return true;
+    }
+    else return false;
+  } catch(err) {
+    return false;
+  }
+}
+
 module.exports = {
-  save,
-  storagePath
+  storagePath,
+  saveSync,
+  putSync,
+  putFromUrl
 };
