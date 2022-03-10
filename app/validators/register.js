fb9884a7e0c0720570143c9a3d91ffbc4fcd2b3e
binhphancreator <tienkhach19@gmail.com>
2022-03-10 17:50:30 +0700

    validation for registering

--- app/validators/register.js
+++ app/validators/register.js
@@ -1,6 +1,6 @@
-var {
-  checkSchema
-} = require('express-validator');
+var checkSchema = require('express-validator').checkSchema;
+const { user } = require('../controllers/auth');
+const User = require('../models/User');
 
 module.exports = checkSchema({
   username: {
@@ -11,10 +11,21 @@ module.exports = checkSchema({
         min: 6
       }
     },
+    errorMessage: 'The username field must be at least 6 character !'
   },
   email: {
     isEmail: true,
     exists: true,
+    custom: {
+      options: async (email) => {
+        var user = await User.findOne({email: email});
+        if (user) 
+          return Promise.reject();
+        return Promise.resolve();
+      },
+      errorMessage: 'The email has already have been taken !'
+    },
+    errorMessage: 'The email field must be valid email address!'
   },
   password: {
     isString: true,
@@ -25,5 +36,6 @@ module.exports = checkSchema({
         max: 20
       }
     },
+    errorMessage: 'The password field must be at least 6 and not be greater than 20 character !'
   }
 }, ['body']);
