78b9df6a22a1663e159d09be4a2a3507fb3fffb3
binhphancreator <tienkhach19@gmail.com>
2022-10-27 15:34:51 +0700

    Fixbug: leave room

--- app/events/disconnecting.js
+++ app/events/disconnecting.js
@@ -14,16 +14,19 @@ function handle(io, socket) {
 
     listRooms.forEach(async roomId => {
 
-      let filter = {id: roomId};
-      let room = await Room.findOne(filter);
-      if(room) {
-        room.users.delete(socketId);
-        await Room.updateOne(filter, {users: room.users});
-        io.sockets.emit('public', {type: 'leave_room', data: {roomId: roomId, user: currentUser}});
-        io.to(roomId).emit('room', {type: 'notification', data: {type: 'error', text: `User ${currentUser.username} has just leave this room.`}});
-        io.to(roomId).emit('room', {type: 'leave_room', data: {socketId: socketId}});
+      try {
+        let room = await Room.findById(roomId);
+        if(room) {
+          room.users.delete(socketId);
+          await Room.findByIdAndUpdate(roomId, {users: room.users});
+          io.sockets.emit('public', {type: 'leave_room', data: {roomId: roomId, user: currentUser}});
+          io.to(roomId).emit('room', {type: 'notification', data: {type: 'error', text: `User ${currentUser.username} has just leave this room.`}});
+          io.to(roomId).emit('room', {type: 'leave_room', data: {socketId: socketId}});
+        }
       }
+      catch(e) {
 
+      }
     });
 
   }
