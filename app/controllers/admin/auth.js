b518a6a0ecc5df9513390fb01d82439dca3ed0e5
thanhlieng <57409019+thanhlieng@users.noreply.github.com>
2022-11-06 01:43:33 +0700

    admin (#36)
    Co-authored-by: binhphancreator <tienkhach19@gmail.com>

--- app/controllers/admin/auth.js
+++ app/controllers/admin/auth.js
@@ -1,10 +1,38 @@
+const User = require("../../models/User");
+const Role = require("../../models/Role");
+const bcrypt = require('bcrypt');
+const uuid = require('uuid');
+
+const dashboardUrl = "/admin/dashboard";
+
 /**
- * 
- * @param {import("express").Request} req 
- * @param {import("express").Response} res 
+ *
+ * @param {import("express").Request} req
+ * @param {import("express").Response} res
  */
 async function showLoginForm(req, res) {
-    res.render('admin/login');
+  res.render("admin/login");
 }
 
-module.exports = {showLoginForm};
+async function loginAdmin(req, res) {
+  try {
+    var user = await User.findOne({ email: req.body.email }).select("+password");
+    if (user && user.role != Role.ADMIN_ROLES) {
+      req.flash("errors", {auth: { msg: "This is not a admin account" }, });
+      res.redirect("/admin/login");
+      return;
+    }
+    if (user && bcrypt.compareSync(req.body.password, user.password)) {
+      req.session.auth = {user: user, token: uuid.v4(), auth: true, };
+      res.redirect(dashboardUrl);
+    } else {
+      req.flash("errors", { auth: { msg: "The email or password is not valid!" }, });
+      res.redirect("/admin/login");
+    }
+  } catch (err) {
+    req.flash("errors", {auth: { msg: "Other Error!!" }, });
+    res.redirect("/admin/login");
+  }
+}
+
+module.exports = { showLoginForm, loginAdmin };
--- app/controllers/auth.js
+++ app/controllers/auth.js
@@ -1,58 +1,63 @@
-var User = require('../models/User');
-var bcrypt = require('bcrypt')
-var dayjs = require('dayjs');
-var uuid = require('uuid');
+var User = require("../models/User");
+var bcrypt = require("bcrypt");
+var dayjs = require("dayjs");
+var uuid = require("uuid");
 
-const homeUrl = '/';
+const homeUrl = "/";
 
 /**
- * 
- * @param {import("express").Request} req 
- * @param {import("express").Response} res 
+ *
+ * @param {import("express").Request} req
+ * @param {import("express").Response} res
  */
 function showLoginForm(req, res) {
-  res.render('login');
+  res.render("login");
 }
 
 /**
- * 
- * @param {import("express").Request} req 
- * @param {import("express").Response} res 
+ *
+ * @param {import("express").Request} req
+ * @param {import("express").Response} res
  */
 async function login(req, res) {
   try {
-    var user = await User.findOne({ email: req.body.email}).select('+password');
+    var user = await User.findOne({ email: req.body.email }).select(
+      "+password"
+    );
     if (user && bcrypt.compareSync(req.body.password, user.password)) {
       req.session.auth = {
         user: user,
         token: uuid.v4(),
         auth: true,
-      }
+      };
       res.redirect(homeUrl);
     } else {
-      req.flash('errors', {auth: {msg: 'The email or password is not valid!'}});
-      res.redirect('/login');
+      req.flash("errors", {
+        auth: { msg: "The email or password is not valid!" },
+      });
+      res.redirect("/login");
     }
   } catch (err) {
-    req.flash('errors', {auth: {msg: 'The email or password is not valid!'}});
-    res.redirect('/login');
+    req.flash("errors", {
+      auth: { msg: "The email or password is not valid!" },
+    });
+    res.redirect("/login");
   }
-  
 }
 
 /**
- * 
- * @param {import("express").Request} req 
- * @param {import("express").Response} res 
+ *
+ * @param {import("express").Request} req
+ * @param {import("express").Response} res
  */
 function showRegisterForm(req, res) {
-  res.render('register');
+  res.render("register");
 }
 
 /**
- * 
- * @param {import("express").Request} req 
- * @param {import("express").Response} res 
+ *
+ * @param {import("express").Request} req
+ * @param {import("express").Response} res
  */
 async function register(req, res) {
   try {
@@ -60,38 +65,38 @@ async function register(req, res) {
       username: req.body.username,
       email: req.body.email,
       password: bcrypt.hashSync(req.body.password, 10),
-    })
-
-    req.flash('success', {
-      message: 'Register an account successfully!'
     });
 
-    res.redirect('/login');
+    req.flash("success", {
+      message: "Register an account successfully!",
+    });
+
+    res.redirect("/login");
   } catch (err) {
     res.send(err);
   }
 }
 
 /**
- * 
- * @param {import("express").Request} req 
- * @param {import("express").Response} res 
+ *
+ * @param {import("express").Request} req
+ * @param {import("express").Response} res
  */
 function user(req, res) {
   res.send(req.session.auth.user);
 }
 
 /**
- * 
- * @param {import("express").Request} req 
- * @param {import("express").Response} res 
+ *
+ * @param {import("express").Request} req
+ * @param {import("express").Response} res
  */
 function logout(req, res) {
   try {
     req.session.destroy(() => {
       res.redirect(homeUrl);
     });
-  } catch(err) {
+  } catch (err) {
     res.status(500).send(err);
   }
 }
@@ -102,5 +107,5 @@ module.exports = {
   showRegisterForm,
   register,
   user,
-  logout
-}
+  logout,
+};
