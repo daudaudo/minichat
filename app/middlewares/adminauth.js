be8088d067fd78c912638f38adf484d7516d3c37
thanh <thanhnn@winds.vn>
2022-11-05 10:58:59 +0700

    admin

---
+++ app/middlewares/adminauth.js
@@ -0,0 +1,16 @@
+const loginUrl = "/admin/login";
+const Role = require("../models/Role");
+
+/**
+ *
+ * @param {import("express").Request} req
+ * @param {import("express").Response} res
+ * @param {import("express").NextFunction} next
+ */
+module.exports = function (req, res, next) {
+  if (!req.session.auth.auth) return res.redirect(loginUrl);
+  if (req.session.auth.user.role != Role.ADMIN_ROLES)
+    return res.redirect(loginUrl);
+  res.locals.auth = req.session.auth;
+  next();
+};
