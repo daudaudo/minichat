d92a9c4bdddf5f248c3aa45124ed0873d54d995b
thanh <thanhnn@winds.vn>
2022-11-06 15:49:38 +0700

    .

--- app/models/User.js
+++ app/models/User.js
@@ -9,55 +9,56 @@ const userSchema = new mongoose.Schema(
       required: true,
       unique: true,
     },
-  'email': {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  'introduction': {
-    type: String,
-  },
-  'password': {
-    type: String,
-    select: false,
-  },
-  'email_verified_at': {
-    type: String,
-  },
-  'picture': {
-    type: String,
-    default: '/images/user.png',
-    required: true
-  },
-  'like': {
-    type: mongoose.SchemaTypes.Map,
-    of: {
-      type: mongoose.SchemaTypes.ObjectId,
-      ref: 'User',
+    email: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    default: {},
-  },
-  'follow': {
-    type: mongoose.SchemaTypes.Map,
-    of: {
-      type: mongoose.SchemaTypes.ObjectId,
-      ref: 'User',
+    introduction: {
+      type: String,
     },
-    default: {},
-  },
-  'role': {
-    type: String,
-    default: Role.NORMAL_ROLES,
-    enum: Role.USER_ROLES,
-  },
-  suspended_at: {
+    password: {
+      type: String,
+      select: false,
+    },
+    email_verified_at: {
+      type: String,
+    },
+    picture: {
+      type: String,
+      default: "/images/user.png",
+      required: true,
+    },
+    like: {
+      type: mongoose.SchemaTypes.Map,
+      of: {
+        type: mongoose.SchemaTypes.ObjectId,
+        ref: "User",
+      },
+      default: {},
+    },
+    follow: {
+      type: mongoose.SchemaTypes.Map,
+      of: {
+        type: mongoose.SchemaTypes.ObjectId,
+        ref: "User",
+      },
+      default: {},
+    },
+    role: {
+      type: String,
+      default: Role.NORMAL_ROLES,
+      enum: Role.USER_ROLES,
+    },
+    suspended_at: {
       type: mongoose.SchemaTypes.Date,
-    }
-}, {
-  timestamps: {
-    createdAt: 'created_at',
-    updatedAt: 'updated_at',
-
+    },
+  },
+  {
+    timestamps: {
+      createdAt: "created_at",
+      updatedAt: "updated_at",
+    },
   }
 );
 
