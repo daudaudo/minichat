882fadc6d56d95cae0c1e277a6bcaecbeed045f9
thanh <thanhnn@winds.vn>
2022-11-06 15:52:35 +0700

    .

--- app/controllers/admin/users.js
+++ app/controllers/admin/users.js
@@ -1,11 +1,9 @@
-const User = require('../../models/User');
+const User = require("../../models/User");
 /**
  *
  * @param {import("express").Request} req
  * @param {import("express").Response} res
  */
-
-
 
 async function banUser(req, res) {
   var filter = { _id: req.body.id };
@@ -16,11 +14,11 @@ async function banUser(req, res) {
 async function deleteUser(req, res) {
   var filter = { _id: req.body.id };
   await User.deleteOne(filter);
+}
 
 async function index(req, res) {
-    const users = await User.find({});
-    res.render('admin/users',{users: users, index: 0});
-
+  const users = await User.find({});
+  res.render("admin/users", { users: users, index: 0 });
 }
 
 module.exports = { index, banUser, deleteUser };
