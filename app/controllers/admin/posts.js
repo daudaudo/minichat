9996fe333cdb2ff4b26777b42cd3e3bbf336652a
root <root@DESKTOP-AKVQD9H.localdomain>
2022-11-10 17:20:13 +0700

    + Add feature ban user, delete post

--- app/controllers/admin/posts.js
+++ app/controllers/admin/posts.js
@@ -9,9 +9,30 @@ const paginate = require('../../global/paginate');
  */
 
 async function index(req, res) {
-    var paginationData = await paginate(req, Post);
+    var paginationData = await paginate(req, Post,{deleted_at: null});
 
     res.render('admin/posts', {...paginationData});
 }
 
-module.exports = {index};
+/**
+ * 
+ * @param {import("express").Request} req 
+ * @param {import("express").Response} res 
+ */
+
+async function deletePost(req, res) {
+    var id = req.params.id;
+    var post = await Post.findById(id);
+    if (!post) {
+        res.status(404);
+        res.send('Not found');
+        return;
+    }
+
+    post.deleted_at = new Date();
+    await post.save();
+
+    res.send(post);
+}
+
+module.exports = {index, deletePost};
