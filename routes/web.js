8f1eb65607b8add2cf05b7ba15eafbcb3a88871b
Nguyễn Ngọc Thành <thanhnn@winds.vn>
2022-11-14 15:21:45 +0700

    .

--- routes/web.js
+++ routes/web.js
@@ -1,55 +1,47 @@
-var express = require("express");
+var express = require('express');
 var router = express.Router();
 
-var homecontroller = require("../app/controllers/home");
-var oauthcontroller = require("../app/controllers/oauth");
-var authcontroller = require("../app/controllers/auth");
-var chatcontroller = require("../app/controllers/chat");
-var usercontroller = require("../app/controllers/user");
-var postcontroller = require("../app/controllers/post");
-var commentcontroller = require("../app/controllers/comment");
+var homecontroller = require('../app/controllers/home');
+var oauthcontroller = require('../app/controllers/oauth');
+var authcontroller = require('../app/controllers/auth');
+var chatcontroller = require('../app/controllers/chat');
+var usercontroller = require('../app/controllers/user');
+var postcontroller = require('../app/controllers/post');
+var commentcontroller = require('../app/controllers/comment');
 
-var registerValidator = require("../app/validators/register");
-var loginValidator = require("../app/validators/login");
-var { validateWithRedirect } = require("../app/middlewares/validate");
-var auth = require("../app/middlewares/auth");
-var adminAuth = require("../app/middlewares/adminauth");
-var webMiddleware = require("../app/middlewares/web");
-var csrf = require("../app/middlewares/csrf");
+var registerValidator = require('../app/validators/register');
+var loginValidator = require('../app/validators/login');
+var { validateWithRedirect } = require('../app/middlewares/validate');
+var auth = require('../app/middlewares/auth');
+var adminAuth = require('../app/middlewares/adminauth');
+var webMiddleware = require('../app/middlewares/web');
+var csrf = require('../app/middlewares/csrf');
 
-router.all("*", webMiddleware);
+router.all('*', webMiddleware);
 
 //Home Router
-router.get("/", homecontroller.home);
-router.get("/chat", chatcontroller.index);
-router.get("/room/:id", chatcontroller.room);
-router.post("/room/files", chatcontroller.files);
+router.get('/', homecontroller.home);
+router.get('/chat', chatcontroller.index);
+router.get('/room/:id', chatcontroller.room);
+router.post('/room/files', chatcontroller.files);
 
 // Oauth Router
-router.get("/google/oauth", oauthcontroller.oauthGoogle);
-router.get("/google/oauth/callback", oauthcontroller.callbackGoogle);
+router.get('/google/oauth', oauthcontroller.oauthGoogle);
+router.get('/google/oauth/callback', oauthcontroller.callbackGoogle);
 
 // Auth Router
-router.get("/register", authcontroller.showRegisterForm);
-router.post("/register",registerValidator,validateWithRedirect(),authcontroller.register);
-router.get("/login", authcontroller.showLoginForm);
-router.post("/login",loginValidator,validateWithRedirect(),authcontroller.login);
-router.get("/user", auth, authcontroller.user);
-router.get("/logout", auth, authcontroller.logout);
+router.get('/register', authcontroller.showRegisterForm);
+router.post('/register',registerValidator,validateWithRedirect(),authcontroller.register);
+router.get('/login', authcontroller.showLoginForm);
+router.post('/login',loginValidator,validateWithRedirect(),authcontroller.login);
+router.get('/user', auth, authcontroller.user);
+router.get('/logout', auth, authcontroller.logout);
 
 //User Router
 
-router.get("/profile", auth, usercontroller.showUpdateProfilesForm);
-router.post("/profile",auth,csrf,require("../app/validators/update-profile"),validateWithRedirect("/profile"),usercontroller.postProfile
+router.get('/profile', auth, usercontroller.showUpdateProfilesForm);
+router.post('/profile',auth,csrf,require('../app/validators/update-profile'),validateWithRedirect('/profile'),usercontroller.postProfile
 );
-
-//Comment Router
-
-router.post("/comment/:id", auth, commentcontroller.post_comment);
-router.post("/update/:id", auth, commentcontroller.update_comment);
-router.get("/comment", commentcontroller.get_comment);
-router.post("/like/:id", auth, commentcontroller.like);
-router.delete("/comment/:id", commentcontroller.delete_comment);
 
 // Post Router
 
@@ -57,15 +49,15 @@ router.get('/posts', postcontroller.index);
 
 // Admin Router
 
-router.get("/admin/login", require("../app/controllers/admin/auth").showLoginForm);
-router.get("/admin/dashboard", adminAuth, require("../app/controllers/admin/dashboard").index);
+router.get('/admin/login', require('../app/controllers/admin/auth').showLoginForm);
+router.get('/admin/dashboard', adminAuth, require('../app/controllers/admin/dashboard').index);
 
-router.get("/admin/users", adminAuth, require("../app/controllers/admin/users").index);
-router.delete("/admin/users/:id", adminAuth, csrf, require("../app/controllers/admin/users").deleteUser);
-router.delete("/admin/users/suspend/:id", adminAuth, csrf, require("../app/controllers/admin/users").suspendUser);
-router.delete("/admin/posts/:id", adminAuth, csrf, require("../app/controllers/admin/posts").deletePost);
+router.get('/admin/users', adminAuth, require('../app/controllers/admin/users').index);
+router.delete('/admin/users/:id', adminAuth, csrf, require('../app/controllers/admin/users').deleteUser);
+router.delete('/admin/users/suspend/:id', adminAuth, csrf, require('../app/controllers/admin/users').suspendUser);
+router.delete('/admin/posts/:id', adminAuth, csrf, require('../app/controllers/admin/posts').deletePost);
 
-router.get("/admin/posts", adminAuth, require("../app/controllers/admin/posts").index);
-router.post("/admin/login", loginValidator, validateWithRedirect(), require("../app/controllers/admin/auth").loginAdmin);
+router.get('/admin/posts', adminAuth, require('../app/controllers/admin/posts').index);
+router.post('/admin/login', loginValidator, validateWithRedirect(), require('../app/controllers/admin/auth').loginAdmin);
 
 module.exports = router;
