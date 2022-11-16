26dcc71799bade178324bf771043a84113e18a2f
root <root@DESKTOP-AKVQD9H.localdomain>
2022-11-16 22:26:16 +0700

    +Show number of like a comment have, post's owner in admin post

--- app/global/paginate.js
+++ app/global/paginate.js
@@ -7,8 +7,9 @@ const perPage = 10;
  * @param {import('mongoose').FilterQuery<T>} filter
  */
 
-async function paginate(req, model, filter) {
+async function paginate(req, model, filter, type) {
     filter = filter ?? {};
+    type = type ?? "";
     var page = parseInt(req.query.page);
     if (isNaN(page) || page < 1)
         page = 1;
@@ -16,8 +17,11 @@ async function paginate(req, model, filter) {
     var lastPage = Math.ceil(numberRecord / perPage);
     if (page > lastPage)
         page = lastPage > 0 ? lastPage : 1;
-    
+
     var data = await model.find(filter).limit(perPage).skip(page - 1);
+
+    if (type == "post")
+        data = await model.find(filter).limit(perPage).skip(page - 1).populate('owner');
 
     return {page, data, lastPage, };
 }
